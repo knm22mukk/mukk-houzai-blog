78ad9be3bf239b496a3dd26c628ee9da6e777495
knm22mukk <knm22mukk@gmail.com>
2021-11-27 14:58:22 +0900

    サイトmetaデータの設定

---
+++ src/data/siteMetadata.js
@@ -0,0 +1,26 @@
+const siteMetadata = {
+  title: '包装資材営業マンむっくのブログサイト',
+  author: '包材営業マンむっく',
+  headerTitle: 'Mukk Blog',
+  description: '包装資材営業マンむっくが商品情報などを発信する個人ブログサイトです。',
+  language: 'ja-JP',
+  siteUrl: 'https://mukk-houzai-blog.vercel.app/',
+  siteRepo: 'https://github.com/knm22mukk/mukk-houzai-blog',
+  siteLogo: 'public/images/logo.png',
+  image: 'public/images/logo.png',
+  socialBanner: 'public/images/twitter_header_photo_2.png',
+  email: 'knm22mukk@gmail.com',
+  github: 'https://github.com/knm22mukk/mukk-houzai-blog',
+  twitter: 'https://twitter.com/mukk_houzai',
+  instagram: 'https://www.instagram.com/mukk_houzai/',
+  facebook: 'https://www.facebook.com/profile.php?id=100073068125069',
+  locale: 'ja-JP',
+  analytics: {
+    // supports plausible, simpleAnalytics or googleAnalytics
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  },
+};
+
+module.exports = siteMetadata;
