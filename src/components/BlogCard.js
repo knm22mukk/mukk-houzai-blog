3061baa87fdcf569e769a46bec14d748e10c792d
knm22mukk <knm22mukk@gmail.com>
2021-11-28 14:45:42 +0900

    ブログページのカードの設定

---
+++ src/components/BlogCard.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from './Link';
+
+export default function BlogCard({ title, description, publishedAt, href, imageSrc }) {
+  return (
+    <Link href={href}>
+      <div className='py-4 px-4 mx-auto md:mx-0 mt-16 w-72 bg-white rounded-xl shadow-lg hover:shadow-2xl transition duration-300 transform hover:scale-110'>
+        <div className=''>
+          <Image src={imageSrc} width={300} height={300} alt={title} className='' />
+          <div className='mt-4 text-center text-blue-500'>
+            <h1 className='text-xl font-bold'>{title}</h1>
+            <p className='mt-4 text-gray-600'>{description}</p>
+            <p className='mt-4 text-gray-600'>{publishedAt}</p>
+            <button className='py-2 px-14 mt-8 mb-4 tracking-widest text-white bg-blue-600 hover:bg-blue-500 rounded-full transition duration-200'>
+              MORE
+            </button>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,19 @@
-import Link from 'next/link';
+import BlogCard from '../components/BlogCard';
 import { client } from '../libs/client';
 
 export default function Blog({ blog }) {
   return (
     <div>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Link href='/'>
-        <a className='text-blue-600'>HOME</a>
-      </Link>
+      {blog.map((blog) => (
+        <BlogCard
+          key={blog.id}
+          imageSrc={blog.image.url}
+          title={blog.title}
+          description={blog.description}
+          href={`/blog/${blog.id}`}
+          publishedAt={blog.publishedAt}
+        />
+      ))}
     </div>
   );
 }
