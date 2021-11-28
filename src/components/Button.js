4f161cc02a153dfdfb297bffcef2c1d402865ba9
knm22mukk <knm22mukk@gmail.com>
2021-11-28 17:03:35 +0900

    404ページの作成、ボタンサイズの調整

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button({ title }) {
   return (
-    <button className='py-2 px-14 my-2 tracking-widest text-white bg-blue-500 hover:bg-blue-700 rounded-full transition duration-200'>
+    <button className='py-2 px-8 my-2 tracking-widest text-white bg-blue-500 hover:bg-blue-700 rounded-full transition duration-200'>
       {title}
     </button>
   );
---
+++ src/pages/404.js
@@ -0,0 +1,25 @@
+import Button from '../components/Button';
+import Link from '../components/Link';
+
+export default function Custom404() {
+  return (
+    <div className='flex flex-col md:flex-row justify-start md:justify-center items-start md:items-center md:mt-24 md:space-x-6'>
+      <div className='pt-6 pb-8 md:space-y-5 space-x-2'>
+        <h1 className='md:px-6 text-6xl md:text-8xl font-extrabold tracking-tight leading-9 text-gray-900 dark:text-gray-100 md:border-r-2'>
+          404
+        </h1>
+      </div>
+      <div className='max-w-md'>
+        <p className='mb-4 text-xl md:text-2xl font-bold leading-normal'>
+          おっと！
+          <br />
+          ページが見つかりませんでした！
+        </p>
+        <p className='mb-8'>URLに間違いがないかご確認ください。</p>
+        <Link href='/'>
+          <Button title='Homeに戻る' />
+        </Link>
+      </div>
+    </div>
+  );
+}
