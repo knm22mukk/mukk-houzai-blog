df16bf2a552ad5405cbdf9a8e8324f695de647c1
knm22mukk <knm22mukk@gmail.com>
2021-12-12 10:31:01 +0900

    GAの設定

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,18 @@
 import 'tailwindcss/tailwind.css';
 import TempleteLayout from '../components/TempleteLayout';
+import GoogleAnalytics from '../components/analytics/GoogleAnalytics';
+import usePageView from '../components/analytics/usePageView';
 
 function MyApp({ Component, pageProps }) {
+  usePageView();
+
   return (
-    <TempleteLayout>
-      <Component {...pageProps} />
-    </TempleteLayout>
+    <>
+      <GoogleAnalytics />
+      <TempleteLayout>
+        <Component {...pageProps} />
+      </TempleteLayout>
+    </>
   );
 }
 
