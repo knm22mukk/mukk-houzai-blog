c987e808654d914b240d05296e2b51ec39167abb
knm22mukk <knm22mukk@gmail.com>
2022-01-10 11:43:44 +0900

    Headerにロゴの追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import headerNavLinks from '../data/headerNavLinks';
 import siteMetadata from '../data/siteMetadata';
 import Link from './Link';
@@ -8,10 +9,19 @@ export default function Header() {
     <header className='flex justify-around items-center py-10 h-32 bg-white'>
       <Link href='/' aria-label='包装資材営業マンむっくのブログサイト'>
         <div className='flex justify-between items-center'>
-          <div className='text-4xl font-bold hover:text-indigo-600'>{siteMetadata.headerTitle}</div>
+          <Image
+            src='/images/logo.png'
+            width={70}
+            height={70}
+            alt='logo'
+            className='rounded-full'
+          />
+          <div className='px-2 text-4xl font-bold hover:text-indigo-600'>
+            {siteMetadata.headerTitle}
+          </div>
         </div>
       </Link>
-      <div className='flex items-center text-base leading-5'>
+      <div className='flex items-center text-xl leading-5'>
         <div className='hidden sm:block mx-2'>
           {headerNavLinks.map((link) => (
             <Link key={link.title} href={link.href} className='p-4 font-bold hover:text-indigo-600'>
