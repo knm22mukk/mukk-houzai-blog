232bdfd2a4f3f81dc80b905fa94cdd42ac5fc0fa
knm22mukk <knm22mukk@gmail.com>
2022-01-11 22:37:21 +0900

    更新情報の追加、HEROの一部変更

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,13 @@ export default function News() {
       <h2 className='py-10 text-4xl font-bold tracking-tight leading-9'>更新情報</h2>
       <div className='justify-center'>
         <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
-          ブログ記事を更新しました。
+          ブログ記事を更新しました。「新年あけましておめでとうございます。」
         </p>
         <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
-          ブログ記事を更新しました。
+          ブログ記事を更新しました。「素人がJAMstackで包材ブログを作ってみた！」
         </p>
         <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
-          ブログ記事を更新しました。
+          ブログサイトをリリースしました。
         </p>
       </div>
     </div>
--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -3,17 +3,17 @@ import Button from './Button';
 
 export default function Header() {
   return (
-    <section className='flex flex-col justify-center items-center py-24'>
+    <section className='flex flex-col justify-center items-center pt-10 pb-24'>
       <Image
-        className='object-cover object-center mb-10 rounded opacity-70'
+        className='object-cover object-center relative mb-10 rounded opacity-90'
         alt='hero'
         src='/images/moldimage.webp'
         height={650}
         width={1280}
       />
-      <div className='mt-10 w-full text-center'>
+      <div className='absolute z-50 mt-10 w-full text-center text-white'>
         <h1 className='mb-4 text-4xl font-bold tracking-tight leading-9 text-center'>MUKK BLOG</h1>
-        <p className='mb-8 leading-relaxed text-gray-600'>
+        <p className='mb-8 leading-relaxed'>
           包装資材、衛生資材、食器、洗剤など様々な商品情報などを発信していきます。
           <br />
           是非ゆっくり見ていってください。
