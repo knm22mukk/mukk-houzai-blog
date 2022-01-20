c22c8213a14c2d2382f3fd75b8646c7dc80b5910
knm22mukk <knm22mukk@gmail.com>
2022-01-20 23:40:54 +0900

    トップページの画像を軽くして画面サイズによって変更するようにした

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -5,13 +5,24 @@ import Container from './Container';
 export default function Header() {
   return (
     <section className='flex flex-col justify-center items-center'>
-      <Image
-        className='object-cover object-center md:relative mb-10 opacity-50'
-        alt='hero'
-        src='/images/moldimage.webp'
-        height={650}
-        width={1280}
-      />
+      <div className='hidden sm:block'>
+        <Image
+          className='object-cover object-center md:relative mb-10 opacity-50'
+          alt='hero'
+          src='/images/moldimage1280x650.webp'
+          height={650}
+          width={1280}
+        />
+      </div>
+      <div className='sm:hidden'>
+        <Image
+          className='object-cover object-center md:relative mb-10 opacity-50'
+          alt='hero'
+          src='/images/moldimage640x350.webp'
+          height={350}
+          width={640}
+        />
+      </div>
       <div className='md:absolute md:z-50 mt-10 w-full text-center md:text-white'>
         <Container>
           <h1 className='mb-4 text-4xl font-bold tracking-tight leading-9 text-center'>
