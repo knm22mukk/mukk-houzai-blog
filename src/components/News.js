a9f53303b058d5c0f1e9f2d076df95c19ba2ce84
knm22mukk <knm22mukk@gmail.com>
2022-01-23 13:33:26 +0900

    更新情報を更新しました。

--- src/components/News.js
+++ src/components/News.js
@@ -8,13 +8,13 @@ export default function News() {
           <h2 className='py-10 text-4xl font-bold tracking-tight leading-9'>更新情報</h2>
           <div className='justify-center'>
             <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
+              パンくずリストを作成し、ページの構造が少しわかりやすくなりました。
+            </p>
+            <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
               お問い合わせページを作成しました。
             </p>
             <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
-              ブログ記事を更新しました。「包装資材の値上げラッシュに関するお話。」
-            </p>
-            <p className='p-6 mt-6 text-gray-500 bg-white rounded-md shadow-lg'>
-              ブログ記事を更新しました。「新年あけましておめでとうございます。」
+              ブログ記事を更新しました。「Fresh+不織布マスクのご紹介」
             </p>
           </div>
         </div>
