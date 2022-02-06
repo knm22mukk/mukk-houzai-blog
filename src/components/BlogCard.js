6e7d6670732ded92896a4d90f28f588bf19de486
knm22mukk <knm22mukk@gmail.com>
2022-02-06 17:04:03 +0900

    ブログタイトルの色を変更しました

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -8,7 +8,9 @@ export default function BlogCard({ title, publishedAt, href, imageSrc }) {
       <div className='p-4 mx-auto mt-6 w-72 sm:w-64 md:w-56 bg-white rounded-xl shadow-lg hover:shadow-2xl transition transform hover:scale-100'>
         <Image src={imageSrc} width={300} height={300} alt={title} />
         <div className='mt-4 text-center'>
-          <h2 className='text-xl font-bold text-indigo-500'>{title}</h2>
+          <h2 className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-indigo-600'>
+            {title}
+          </h2>
           <p className='mt-2 text-gray-600'>
             <Date dateString={publishedAt} />
           </p>
